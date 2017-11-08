7e3847157f96473ef0fca7be0fad3038e42c7417
Eduardo Ferreia <edu.private@gmail.com>
2017-11-07 22:19:50 -0200

    config parser

--- arit.sh
+++ arit.sh
@@ -2,6 +2,6 @@
 
 BIGNUM=1024
 let RESULT=$BIGNUM/16
-RESULT=´expr $BIGNUM / 16´
-RESULT=´echo "$BIGNUM / 16´" | bc´
+RESULT='expr $BIGNUM / 16'
+RESULT='echo "$BIGNUM / 16" | bc'
 let foo=RANDOM%10; echo $foo
